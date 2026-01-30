01df2c6bedab4b8b14e888f0b9a5319f15068f7c
Paul Gundarapu <leogps@gmail.com>
2026-01-29 21:25:11 -0800

    initial draft.

---
+++ setup-credential-manager.sh
@@ -0,0 +1,35 @@
+#!/bin/env bash
+
+set -e
+
+install_dependencies() {
+	apt install -y gnupg2 pinentry-curses	
+}
+
+restart_gpg_agent() {
+	gpgconf --kill gpg-agent
+	gpgconf --launch gpg-agent
+}
+
+setup() {
+	install_dependencies
+
+	gpg --full-generate-key
+
+	cp ./git-credential-gpg $HOME/.git-credential-gpg
+
+	chmod +x $HOME/.git-credential-gpg
+
+	git config --global credential.helper "$HOME/.git-credential-gpg"
+
+	mkdir -p $HOME/.gnupg
+	cp ./gpg/gpg-agent.conf $HOME/.gnupg/gpg-agent.conf
+	cp ./gpg/gpg.conf $HOME/.gnupg/gpg.conf
+
+	chmod 700 $HOME/.gnupg
+	chmod 600 $HOME/.gnupg/*
+
+	restart_gpg_agent
+}
+
+setup
